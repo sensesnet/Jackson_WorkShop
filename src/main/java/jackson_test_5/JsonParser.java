3f83d1a18bee9537a495ee9ff5ede3bad8a7ac00
sensesnet <git1202>
2019-05-19 22:35:40 +0300

    Json to Map

--- src/main/java/jackson_test_5/JsonParser.java
+++ src/main/java/jackson_test_5/JsonParser.java
@@ -1,12 +1,15 @@
 package jackson_test_5;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import pojos.Car;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class JsonParser {
     public static void main(String[] args) throws IOException {
+
 
         String jsonArray = "[{\"brand\":\"ford\"},{\"brand\":\"maserati\"}]";
         ObjectMapper objectMapper = new ObjectMapper();
@@ -14,5 +17,13 @@ public class JsonParser {
         for (Car car : carArray) {
             System.out.println(car.getBrand());
         }
+
+//        Read map from JSON String
+        String jsonObject = "{\"brand\":\"ford\",\"doors\":5}";
+        Map<String, Object> objectMap = objectMapper.readValue(jsonObject,
+                new TypeReference<Map<String, Object>>() {
+                });
+        System.out.println("Map: " + objectMap);
+
     }
 }
